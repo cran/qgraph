f049130b795b3a52a6c5580b3644086d3f0312d1
Sacha Epskamp <sacha.epskamp@gmail.com>
2011-01-08 08:47:48 +0000

    version 0.4.0

---
+++ R/qgraph_pca.R
@@ -0,0 +1,22 @@
+## qgraph.pca
+
+# EXPLORATORY FACTOR ANALYSIS
+# Using factanal(..)
+
+# all arguments of qgraph.loadings are included. plus:
+
+# cor: correlation matrix on which to perform the EFA.
+# factors: vector with how much factors are to be used
+# rotation: rotations to be used.
+
+qgraph.pca=function(cor,factors=1,rotation="promax",...) {
+
+require(psych)
+
+fact=principal(cor,factors,rotate=rotation)
+loadings=as.matrix(loadings(fact)[1:nrow(cor),1:factors])
+
+qgraph.loadings(loadings,model="formative",...) 
+	
+
+}
